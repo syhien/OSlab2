3e7d33dd47cd47b98e144c92ca2c67320aca610d
syhien <syhien@outlook.at>
2021-11-08 17:45:03 +0800

    优化UI

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/widget"
 	"image/color"
 	"strconv"
 	"sync"
@@ -28,8 +27,10 @@ func main() {
 }
 
 func floodOne(elevator fyne.App, elevatorStatus ElelvatorStatus) {
-	winOne := elevator.NewWindow("Flood 1")
+	winOne := elevator.NewWindow("1")
 	winOne.Resize(fyne.NewSize(200, 200))
+	titleText := canvas.NewText("Flood 1 Panel", color.Black)
+	titleText.TextStyle = fyne.TextStyle{Bold: true}
 	statusText := canvas.NewText("", color.Black)
 	go func() {
 		for true {
@@ -49,19 +50,65 @@ func floodOne(elevator fyne.App, elevatorStatus ElelvatorStatus) {
 			statusText.Text = tmpLabel
 		}
 	}()
-	content := container.NewVBox(canvas.NewText("Flood 1 Panel", color.Black), statusText)
+	content := container.NewVBox(titleText, statusText)
 	winOne.SetContent(content)
 	winOne.Show()
 }
 
 func floodTwo(elevator fyne.App, elevatorStatus ElelvatorStatus) {
-	winTwo := elevator.NewWindow("Flood 2")
-	winTwo.SetContent(widget.NewLabel("2层电梯面板"))
+	winTwo := elevator.NewWindow("2")
+	winTwo.Resize(fyne.NewSize(200, 200))
+	titleText := canvas.NewText("Flood 2 Panel", color.Black)
+	titleText.TextStyle = fyne.TextStyle{Bold: true}
+	statusText := canvas.NewText("", color.Black)
+	go func() {
+		for true {
+			tmpLabel := "> "
+			elevatorStatus.lock.RLock()
+			tmpLabel += strconv.Itoa(elevatorStatus.position) + " "
+			if elevatorStatus.isMoving {
+				if elevatorStatus.isUp {
+					tmpLabel += "UP"
+				} else {
+					tmpLabel += "DOWN"
+				}
+			} else {
+				tmpLabel += ""
+			}
+			elevatorStatus.lock.RUnlock()
+			statusText.Text = tmpLabel
+		}
+	}()
+	content := container.NewVBox(titleText, statusText)
+	winTwo.SetContent(content)
 	winTwo.Show()
 }
 
 func floodThree(elevator fyne.App, elevatorStatus ElelvatorStatus) {
-	winThree := elevator.NewWindow("Flood 3 Controller")
-	winThree.SetContent(widget.NewLabel("3层电梯面板"))
+	winThree := elevator.NewWindow("3")
+	winThree.Resize(fyne.NewSize(200, 200))
+	titleText := canvas.NewText("Flood 3 Panel", color.Black)
+	titleText.TextStyle = fyne.TextStyle{Bold: true}
+	statusText := canvas.NewText("", color.Black)
+	go func() {
+		for true {
+			tmpLabel := "> "
+			elevatorStatus.lock.RLock()
+			tmpLabel += strconv.Itoa(elevatorStatus.position) + " "
+			if elevatorStatus.isMoving {
+				if elevatorStatus.isUp {
+					tmpLabel += "UP"
+				} else {
+					tmpLabel += "DOWN"
+				}
+			} else {
+				tmpLabel += ""
+			}
+			elevatorStatus.lock.RUnlock()
+			statusText.Text = tmpLabel
+		}
+	}()
+	content := container.NewVBox(titleText, statusText)
+	winThree.SetContent(content)
 	winThree.Show()
 }
